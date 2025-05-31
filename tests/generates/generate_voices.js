c5ad0eddb6c33ff03bcee2ffda73b1d8d02eba63
DeepSparks <chir98637@gmail.com>
2025-05-31 23:13:51 +0900

    테스트 스크립트 구조화 작업

--- tests/generates/generate_voices.js
+++ tests/generates/generate_voices.js
@@ -1,4 +1,4 @@
-import TestUtil from './test_util.js';
+import TestUtil from '../test_util.js';
 
 async function main() {
     await TestUtil.jsonRequest('http://127.0.0.1:3000/addCharacters', {
--- tests/styles/add_styles.js
+++ tests/styles/add_styles.js
@@ -1,4 +1,4 @@
-import TestUtil from './test_util.js';
+import TestUtil from '../test_util.js';
 
 TestUtil.jsonRequest('http://127.0.0.1:3000/addStyles', {
     resource_name: 'test_resource',
