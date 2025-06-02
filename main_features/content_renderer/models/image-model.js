862f159f0c9826ad4514719668ace7f9eb1cea6c
DeepSparks <chir98637@gmail.com>
2025-06-02 13:51:05 +0900

    NSFW 키워드가 Common Prompt에 추가되도록 변경됨

--- main_features/content_renderer/models/image-model.js
+++ main_features/content_renderer/models/image-model.js
@@ -28,18 +28,18 @@ class ImageModel extends BaseModel {
         const first_nsfw_keyword = this.nsfw.split(',')[0].trim();
         const left_nsfw_keywords = this.nsfw.split(',').slice(1).map(keyword => keyword.trim());
         if(first_nsfw_keyword === 'none') {
-            this.nsfw = left_nsfw_keywords.join(', ');
             this.common_negative_prompt = 'nsfw';
+            if(left_nsfw_keywords.length > 0) this.common_prompt = left_nsfw_keywords.join(', ') + ", " + this.common_prompt;
         } else if(FrontConfig.MAP_KEYWORDS.NSFW[first_nsfw_keyword]) {
-            if(FrontConfig.IS_NOT_USE_NSFW_PROMPT_MAP) {
-                this.common_prompt = FrontConfig.DEFAULT_KEYWORDS.NSFW.join(', ') + ", " + this.common_prompt;
+            if(!FrontConfig.IS_NOT_USE_NSFW_PROMPT_MAP) {
+                this.nsfw = FrontConfig.MAP_KEYWORDS.NSFW[first_nsfw_keyword];
+                if(left_nsfw_keywords.length > 0) this.nsfw = this.nsfw + ", " + left_nsfw_keywords.join(', ');
             }
-            else {
-                this.common_prompt = FrontConfig.DEFAULT_KEYWORDS.NSFW.join(', ') + ", " + this.common_prompt;
-                this.nsfw = FrontConfig.MAP_KEYWORDS.NSFW[first_nsfw_keyword] + ", " + left_nsfw_keywords.join(', ');
-            }
-        }
-        this.character_prompt = [this.nsfw, this.pose, this.expression, this.etc_char].filter(attr => attr).join(', ') || '';
+            this.common_prompt = FrontConfig.DEFAULT_KEYWORDS.NSFW.join(', ') + ", " + this.nsfw + ", " + this.common_prompt;
+        } else {
+            this.common_prompt = this.nsfw  + ", " + this.common_prompt;
+        } 
+        this.character_prompt = [this.pose, this.expression, this.etc_char].filter(attr => attr).join(', ') || '';
     }
 
     getDeterministicRandomView(attributes) {
